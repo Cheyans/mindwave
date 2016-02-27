76dfddee04de2e0e476cf345ff90843ccdee9af9
Cheyan <cheyan.set@gmail.com>
2016-02-27 12:51:00 -0500

    proper data parsing

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ var neurosky = require('node-neurosky');
 var WebSocketServer = require('ws').Server;
 
 var client = neurosky.createClient({
-	appName:'NodeNeuroSky',
-	appKey:'0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+  appName: 'NodeNeuroSky',
+  appKey: '0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
 })
 
 current_tick = 0;
@@ -12,44 +12,47 @@ attention = [];
 meditation = [];
 
 // bind receive data event
-client.on('data',function(data){
+client.on('data', function(data) {
   // if websocket server is running
-  if(wss){
+  if (wss) {
     // broadcast this latest data packet to all connected clients
     wss.broadcast(data);
   }
   //var high_alpha = [];
   //var low_alpha = [];
-  
 
 
-  //console.log("whoops");
-  if(data.poorSignalLevel < 25 && data.eSense.attention > 0 
-    && data.eSense.meditation > 0){
+	if(data.poorSignalLevel) {
+		console.log("Signal Strength: " + parseInt(data.poorSignalLevel));
+	} else if (data.blinkStrength) {
+		console.log(data);
+	} else if (data.poorSignalLevel < 25 && data.eSense.attention > 0 && data.eSense.meditation > 0) {
     current_tick++;
     attention.push(data.eSense.attention);
     meditation.push(data.eSense.meditation);
-    if(current_tick % tick == 0){
-      //find average 
-      var med_sum = 0;  
-      for(var data : meditation)
-        med_sum += data.meditation;
-      var atten_sum = 0;  
-      for(var data : attention)
-        atten_sum += data.attention;
-      
-      var med_avg = med_sum/tick;
-      var atten_avg = med_sum/tick;
-
-      attention = [];
-      meditation = [];
+    if (current_tick % tick == 0) {
+      //find average
+      var med_sum = 0;
+      meditation.forEach(function(val) {
+        med_sum += val;
+      });
+      var atten_sum = 0;
+			attention.forEach(function(val) {
+        atten_sum += val;
+      });
+      var med_avg = med_sum / tick;
+      var atten_avg = atten_sum / tick;
 
       //then push results to a command on the device
-      console.log("average meditation was"+ med_avg);
-      console.log("average attention was"+ atten_avg);
+      console.log("Avg Med: " + parseInt(med_avg));
+      console.log("Avg Atten: " + parseInt(atten_avg));
+
+			//empty array
+			attention = [];
+			meditation = [];
     }
 
-    }
+  }
 
 });
 // initiate connection
@@ -58,20 +61,22 @@ client.connect();
 
 /** BEGIN start our websocket server **/
 // start websocket server to broadcast
-var wss = new WebSocketServer({port: 8080});
+var wss = new WebSocketServer({
+  port: 8080
+});
 
 // broadcast function (broadcasts message to all clients)
 wss.broadcast = function(data) {
-    for(var i in this.clients)
-        this.clients[i].send(data);
+  for (var i in this.clients)
+    this.clients[i].send(data);
 };
 
 // bind each connection
 wss.on('connection', function(ws) {
-    ws.on('message', function(message) {
-      //filter out any bad data
+  ws.on('message', function(message) {
+    //filter out any bad data
 
-      console.log('[CLIENT]  %s', message);
-    });
-    ws.send('You are connected to Mindwave Mobile');
+    console.log('[CLIENT]  %s', message);
+  });
+  ws.send('You are connected to Mindwave Mobile');
 });
